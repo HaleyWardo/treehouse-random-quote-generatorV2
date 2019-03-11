7cfb4264d19cae401e115d2e44de8cc8546871cb
Haley <hayevans18@gmail.com>
2019-03-11 10:55:47 -0400

    Update printQuote function and add getRandomColor function

--- js/script.js
+++ js/script.js
@@ -3,83 +3,94 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+// An array of objects with quote information
 const quotes = [
   {
     quote: 'Clever girl',
-    source: 'Robert Muldoon'
+    source: 'Robert Muldoon',
+    year: 1993,
+    genre: 'Adventure, Sci-Fi, Thriller'
   },
   {
     quote: 'Welcome... to Jurassic Park',
-    source: 'John Hammond'
+    source: 'John Hammond',
+    citation: 'Jurassic Park',
   },
   {
-    quote: 'Help me, Obi-Wan Kenobi. You’re my only hope.',
-    source: 'Leia Organa'
+    quote: 'Help me, Obi-Wan Kenobi. You’re my only hope',
+    source: 'Leia Organa',
+    citation: ' Star Wars Episode IV: A New Hope',
+    year: 1977
   },
   {
     quote: "I'm one with the Force. The Force is with me",
-    source: 'Chirrut Îmwe'
+    source: 'Chirrut Îmwe',
+    citation: 'Star Wars: Rogue One',
+    year: 2016,
+    genre: 'Action, Adventure, Sci-Fi'
   },
   {
-    qoute: 'Do. Or do not. There is no try', 
-    source: 'Yoda'
+    quote: 'Do. Or do not. There is no try', 
+    source: 'Yoda',
+    citation: 'Star Wars Episode V: The Empire Strikes Back',
+    year: 1980
   }
 ];
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+const backgroundColors = ['#0e2f44', '#a27268', '#a7adba', '#414042', '#2f7585', '#1E8856'];
 
-const getRandomQuote = () => {
-  const randomNumber = quotes[Math.ceil(Math.random() *  quotes.length - 1)];
-  return randomNumber;
-}
+/**
+ * getRandomQuote generates a new random quote
+ */
+let getRandomQuote = () => quotes[Math.floor(Math.random() * quotes.length)];
 
+/**
+ * getRandomBackgroundColor generates a new random background color
+ */
+let getRandomColor = () => backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+
+/**
+ * printQuote calls the getRandomQuote function and uses dot notation to access key/value pairs.
+ * With access to the random quote, we are manipulating the DOM to inject the new quote.
+ * Lastly, we are changing the background color to a random color
+ */
 const printQuote = () => {
   const randomQuote = getRandomQuote();
-  const quoteHtml = '';
+  const randomColor = getRandomColor();
+
+  document.body.style.background = randomColor;
+
+  let quoteHtml = `
+    <p class="quote">${randomQuote.quote}</p>
+    <p class="source">${randomQuote.source}
+  `;
+
+  if (randomQuote.citation) {
+    quoteHtml += `
+      <span class="citation">${randomQuote.citation}</span>
+    `;
+  }
+
+  if (randomQuote.year) {
+    quoteHtml += `
+      <span class="year">${randomQuote.year}</span>
+    `;
+  }
+
+  if (randomQuote.genre) {
+    quoteHtml += `
+      <span class="genre">${randomQuote.genre}</span>
+    `;
+  }
+
+  quoteHtml += '</p>';
+
+  document.querySelector('#quote-box').innerHTML = quoteHtml;
 }
 
+// set interval method so that a new quote is generated every 10 seconds
+setInterval(printQuote, 10000);
 
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
-// document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+/* event listener to respond to "Show another quote" button clicks
+when user clicks anywhere on the button, the "printQuote" function is called */
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
